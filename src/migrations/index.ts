1557a19bd335a0a15a7fbffd91368683361da881
Zaid-deve <patelzaid721@gmail.com>
2026-03-21 23:12:54 +0530

    add contact_leads migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260321_174146 from './20260321_174146';
+
+export const migrations = [
+  {
+    up: migration_20260321_174146.up,
+    down: migration_20260321_174146.down,
+    name: '20260321_174146'
+  },
+];
