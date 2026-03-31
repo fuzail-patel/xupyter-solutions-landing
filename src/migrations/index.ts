a103a5da4af90c919410449345f163b968d4e031
Zaid-deve <patelzaid721@gmail.com>
2026-04-01 03:55:14 +0530

    updated migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260321_175752 from './20260321_175752';
-
-export const migrations = [
-  {
-    up: migration_20260321_175752.up,
-    down: migration_20260321_175752.down,
-    name: '20260321_175752'
-  },
-];
